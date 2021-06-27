abaa086f26a617457bec9fb5ef8f7c191fbf21a9
谢在奎 <jackshea@163.com>
2021-06-27 20:22:58 +0800

    初始提交

---
+++ NLogClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using NLog;
+using System;
+using System.Threading;
+
+namespace NLogClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogManager.Configuration.Variables["UserId"] = "xzk" + new Random((int)DateTime.Now.Ticks).Next(100);
+            var logger = LogManager.GetCurrentClassLogger();
+            logger.Info("Test Nlog level.");
+            logger.Trace("Trace" );
+            logger.Debug("Debug" );
+            logger.Info("Info");
+            logger.Warn("Warn" );
+            logger.Error("Error");
+            logger.Fatal("Fatal");
+
+            for (int i = 0; i < 100; i++)
+            {
+                logger.Info( i);
+                Thread.Sleep(1000);
+            }
+
+            LogManager.Flush((a) =>
+            {
+                LogManager.Shutdown();
+            }, TimeSpan.FromSeconds(2));
+
+        }
+    }
+}
